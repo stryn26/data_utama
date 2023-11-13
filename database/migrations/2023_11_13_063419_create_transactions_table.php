b2417682f4bf6fb8324f91f87cf93887d85439b1
stryn26 <sutrionoarif@gmail.com>
2023-11-13 13:48:39 +0700

    intial commit

---
+++ database/migrations/2023_11_13_063419_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference_no');
+            $table->string('price');
+            $table->integer('quantity');
+            $table->string('payment_amount');
+            $table->foreignId('product_id');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
